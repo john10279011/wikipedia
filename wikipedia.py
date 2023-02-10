4f4ec16d11522959c3406422ae5bb348399719a3
John Mukundi Mugure <96171675+john10279011@users.noreply.github.com>
2023-02-10 10:44:10 +0300

    Update wikipedia.py

--- wikipedia.py
+++ wikipedia.py
@@ -13,5 +13,30 @@ items = page.find_all("li", class_="mw-search-result mw-search-result-ns-0")
 for item in items:
     name = item.find("div", class_="mw-search-result-heading").find("a").text
     desc = item.find("div", class_="searchresult").text
-    link = item.find("div", class_="mw-search-result-heading").find("a")["href"]
-    print(name, "- | -", desc, "- | -", link)
+    link = "https://en.wikipedia.org" + str(
+        item.find("div", class_="mw-search-result-heading").find("a")["href"]
+    )
+    print(link)
+
+
+def getpage(url):
+    page = sesh.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    return soup
+
+
+def get_data(soup):
+    items = page.find_all("li", class_="mw-search-result mw-search-result-ns-0")
+    for item in items:
+        name = item.find("div", class_="mw-search-result-heading").find("a").text
+        desc = item.find("div", class_="searchresult").text
+        link = "https://en.wikipedia.org" + str(
+            item.find("div", class_="mw-search-result-heading").find("a")["href"]
+        )
+        print(name, link, desc)
+
+    return
+
+
+soup = getpage(url)
+get_data(soup)
