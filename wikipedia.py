32aa798bc3e8e3025951e2c74f26f2be736898c0
john <johnmugure659@gmail.com>
2023-02-10 09:39:23 +0300

    wikipedia

---
+++ wikipedia.py
@@ -0,0 +1,5 @@
+from requests_html import HTMLSession
+from bs4 import Beautifulsoup
+import pandas as pd
+
+url = "https://en.wikipedia.org/wiki/Special:Search?go=Go&search=latest+trending+movies+on+hollywood&ns0=1"
