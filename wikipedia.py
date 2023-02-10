895592d7c73e034934cd8a272d39152cadfe6141
john <johnmugure659@gmail.com>
2023-02-10 10:49:01 +0300

    wikipedia

--- wikipedia.py
+++ wikipedia.py
@@ -9,16 +9,6 @@ sesh = HTMLSession()
 page = BeautifulSoup(sesh.get(url).content, "html.parser")
 
 
-items = page.find_all("li", class_="mw-search-result mw-search-result-ns-0")
-for item in items:
-    name = item.find("div", class_="mw-search-result-heading").find("a").text
-    desc = item.find("div", class_="searchresult").text
-    link = "https://en.wikipedia.org" + str(
-        item.find("div", class_="mw-search-result-heading").find("a")["href"]
-    )
-    print(link)
-
-
 def getpage(url):
     page = sesh.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
